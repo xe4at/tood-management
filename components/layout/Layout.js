10d957bafbade9ad46a1880dabffd4648c296163
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-05-15 20:54:23 +0330

    development signin api with next-auth]

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { VscListSelection } from "react-icons/vsc";
 import { BiMessageSquareAdd } from "react-icons/bi";
 import { RxDashboard } from "react-icons/rx";
+
 function Layout({ children }) {
   return (
     <div className="container">
@@ -16,12 +17,10 @@ function Layout({ children }) {
               <VscListSelection />
               <Link href="/">Todos</Link>
             </li>
-
             <li>
               <BiMessageSquareAdd />
               <Link href="/add-todo">Add Todo</Link>
             </li>
-
             <li>
               <RxDashboard />
               <Link href="/profile">Profile</Link>
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import User from "../../../../models/User";
+import { verifyPassword } from "../../../../utils/auth";
+import connectDB from "../../../../utils/connectDB";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials", 
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials || {}; 
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Error in connecting to DB!");
+        }
+
+        if (!email || !password) {
+          throw new Error("Invalid Data!");
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) throw new Error("User doesn't exist!");
+
+        const isValid = await verifyPassword(password, user.password);
+
+        if (!isValid) throw new Error("Username or password is incorrect!");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
