69859922c294ba6ae6c45d6e95dcba5009e9eb82
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-05-25 19:27:30 +0330

    show user information

--- components/template/ProfilePage.js
+++ components/template/ProfilePage.js
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import ProfileForm from "../module/ProfileForm";
+import ProfileData from "../module/ProfileData";
 
 function ProfilePage() {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  const fetchProfile = async () => {
+    const res = await fetch("/api/profile");
+    const data = await res.json();
+    if (data.status === "success" && data.data.name && data.data.lastName) {
+      setData(data.data);
+    }
+  };
 
   const submitHandler = async () => {
     const res = await fetch("/api/profile", {
@@ -23,15 +37,19 @@ function ProfilePage() {
         <CgProfile />
         Profile
       </h2>
-      <ProfileForm
-        name={name}
-        lastName={lastName}
-        password={password}
-        setName={setName}
-        setLastName={setLastName}
-        setPassword={setPassword}
-        submitHandler={submitHandler}
-      />
+      {data ? (
+        <ProfileData data={data} />
+      ) : (
+        <ProfileForm
+          name={name}
+          lastName={lastName}
+          password={password}
+          setName={setName}
+          setLastName={setLastName}
+          setPassword={setPassword}
+          submitHandler={submitHandler}
+        />
+      )}
     </div>
   );
 }
